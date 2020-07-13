72a1053b711f5d9ab675c4a41ac7713c120fe1ec
HoseungShin <nwdt3309@gmail.com>
2020-07-14 00:45:58 +0900

    [blog] material-ui 추가

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+
+const Home = () => {
+    return (
+        <div style={{width: '1000px', height: '1000px'}}>
+        </div>
+    )
+}
+
+export default Home;
